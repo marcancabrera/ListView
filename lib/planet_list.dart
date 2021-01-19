b5a6b63cccd9ea9314d9590e1e7940bfc11c04f6
Marc Cabrera <marcandea.cabrera@fundacion-jala.org>
2021-01-19 11:15:04 -0400

    Implement PlanetList

--- lib/planet_list.dart
+++ lib/planet_list.dart
@@ -1,1 +1,24 @@
+import 'package:ListView/planet.dart';
 import 'package:flutter/material.dart';
+
+class PlanetList extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    final planetList = Container(
+      height: 350.0,
+      child: ListView(
+        padding: EdgeInsets.all(25.0),
+        scrollDirection: Axis.horizontal,
+        children: [
+          PlanetImage('assets/img/earth.jpg'),
+          PlanetImage('assets/img/earth.jpg'),
+          PlanetImage('assets/img/earth.jpg'),
+          PlanetImage('assets/img/earth.jpg')
+        ],
+      ),
+    );
+    return planetList;
+  }
+
+}
